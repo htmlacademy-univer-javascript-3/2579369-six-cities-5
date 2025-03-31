f283daee6be3b46a2ffd1ef7c06bd3cf1e75e58b
Suninall <sanina-nsk@mail.ru>
2025-03-31 16:05:23 +0700

    подключает комментарии к offer-page

--- src/components/offer-page/reviews-list.tsx
+++ src/components/offer-page/reviews-list.tsx
@@ -1,17 +1,21 @@
 import ReviewItem from './review-item';
-import { Review } from '../../types/reviews';
+import { Reviews } from '../../mock/reviews';
+import ReviewForm from '../review-form/review-form';
 
-type ReviewsListProp = {
-  reviews: Review[];
-}
+const ReviewsList = (): JSX.Element => {
+  const reviewsSorted = [...Reviews].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()).slice(0, 10);
 
-const ReviewsList = ({reviews}: ReviewsListProp): JSX.Element => (
-
-  <ul className="reviews__list">
-    {reviews.map((review) => (
-      <ReviewItem key={review.id} review={review} />
-    ))}
-  </ul>
-);
+  return(
+    <section className="offer__reviews reviews">
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{Reviews.length}</span></h2>
+      <ul className="reviews__list">
+        {reviewsSorted.map((review) => (
+          <ReviewItem key={review.id} review={review} />
+        ))}
+      </ul>
+      <ReviewForm />
+    </section>
+  );
+};
 
 export default ReviewsList;
