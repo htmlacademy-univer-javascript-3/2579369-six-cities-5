37595e5e446c690ddaa78ab65d2c3769e2e11566
Suninall <sanina-nsk@mail.ru>
2025-04-13 13:05:43 +0700

    создает действие для проверки авторизации

--- src/components/store/api-action.ts
+++ src/components/store/api-action.ts
@@ -3,8 +3,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../../types/state';
 //import { saveToken, dropToken } from '../services/token';
 import { OfferPreview } from '../../types/offers-preview';
-import {APIRoute } from '../../const/const';
-import { loadOffers, setOffersDataLoadingStatus } from './action';
+import {APIRoute, AuthorizationStatus } from '../../const/const';
+import { loadOffers, setOffersDataLoadingStatus, requireAuthorization } from './action';
 
 export const fetchOffers = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -17,5 +17,21 @@ export const fetchOffers = createAsyncThunk<void, undefined, {
     const {data} = await api.get<OfferPreview[]>(APIRoute.Offers);
     dispatch(setOffersDataLoadingStatus(false));
     dispatch(loadOffers(data));
+  },
+);
+
+export const ckeckAuthAction = createAsyncThunk<void, undefined,{
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
   },
 );
