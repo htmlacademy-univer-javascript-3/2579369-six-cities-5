605a7307233274e55b3d7f1f37ed20361467fcc2
Suninall <sanina-nsk@mail.ru>
2025-04-19 23:10:12 +0700

    исправляет знак евро в цене офера

--- src/components/offer-page/offer-page.tsx
+++ src/components/offer-page/offer-page.tsx
@@ -133,7 +133,7 @@ const OfferPage = (): JSX.Element => {
                 </li>
               </ul>
               <div className="offer__price">
-                <b className="offer__price-value">&euro;${CurrentOffer?.price}</b>
+                <b className="offer__price-value">&euro;{CurrentOffer?.price}</b>
                 <span className="offer__price-text">&nbsp;night</span>
               </div>
               <div className="offer__inside">
