215f9cec4d9965ae53af6a10f3bf0d89b16a18de
Suninall <sanina-nsk@mail.ru>
2025-04-21 10:05:09 +0700

    добавляет spinner в offer-page

--- src/components/offer-page/offer-page.tsx
+++ src/components/offer-page/offer-page.tsx
@@ -17,6 +17,7 @@ import OfferInsideList from './offer-inside-list';
 import { getDistance } from '../../utils/offer-page';
 import { AppRoute, AuthorizationStatus } from '../../const/const';
 import { updateFavorites } from '../store/api-action';
+import Spinner from '../spinner/spinner';
 
 
 const OfferPage = (): JSX.Element => {
@@ -46,13 +47,13 @@ const OfferPage = (): JSX.Element => {
     }
   },[dispatch,id]);
 
-  const сurrentOffer = useAppSelector((state) => state.currentOffer);
-  const сurrentReviews = useAppSelector((state) => state.currentReviews) ?? [];
+  const currentOffer = useAppSelector((state) => state.currentOffer);
+  const currentReviews = useAppSelector((state) => state.currentReviews) ?? [];
   const allNearOffers = useAppSelector((state) => state.nearOffers);
   const favoritesOffers = useAppSelector((state) => state.favorites);
 
   const nearOffers = useMemo(() => {
-    if (!сurrentOffer) {
+    if (!currentOffer) {
       return [];
     }
 
@@ -60,27 +61,32 @@ const OfferPage = (): JSX.Element => {
       .map((offer) => ({
         ...offer,
         distance: getDistance(
-          сurrentOffer.location.latitude,
-          сurrentOffer.location.longitude,
+          currentOffer.location.latitude,
+          currentOffer.location.longitude,
           offer.location.latitude,
           offer.location.longitude
         )
       }))
       .sort((a, b) => a.distance - b.distance)
       .slice(0, 3);
-  }, [сurrentOffer, allNearOffers]);
+  }, [currentOffer, allNearOffers]);
 
   const handleFavoriteClick = () => {
     if (authorizationStatus !== AuthorizationStatus.Auth) {
       navigate(AppRoute.Login);
       return;
     }
-    if(сurrentOffer){
-      dispatch(updateFavorites({offerId: сurrentOffer.id, status: сurrentOffer?.isFavorite ? 0 : 1}));
+    if(currentOffer){
+      dispatch(updateFavorites({offerId: currentOffer.id, status: currentOffer.isFavorite ? 0 : 1}));
     }
 
   };
 
+  if(!currentOffer){
+    return(
+      <Spinner/>
+    );
+  }
   return(
 
     <div className="page">
@@ -91,25 +97,25 @@ const OfferPage = (): JSX.Element => {
         <section className="offer">
           <div className="offer__gallery-container container">
             <div className="offer__gallery">
-              {сurrentOffer && (
+              {currentOffer && (
                 <div className="offer__gallery">
-                  <OfferImgList offer={сurrentOffer} />
+                  <OfferImgList offer={currentOffer} />
                 </div>
               )}
             </div>
           </div>
           <div className="offer__container container">
             <div className="offer__wrapper">
-              {сurrentOffer?.isPremium && (
+              {currentOffer.isPremium && (
                 <div className="offer__mark">
                   <span>Premium</span>
                 </div>
               )}
               <div className="offer__name-wrapper">
                 <h1 className="offer__name">
-                  {сurrentOffer?.title}
+                  {currentOffer.title}
                 </h1>
-                <button className={`offer__bookmark-button button ${сurrentOffer?.isFavorite ? 'offer__bookmark-button--active' : ''}`} type="button" onClick={handleFavoriteClick}>
+                <button className={`offer__bookmark-button button ${currentOffer.isFavorite ? 'offer__bookmark-button--active' : ''}`} type="button" onClick={handleFavoriteClick}>
                   <svg className="offer__bookmark-icon" width="31" height="33">
                     <use xlinkHref="#icon-bookmark"></use>
                   </svg>
@@ -118,44 +124,44 @@ const OfferPage = (): JSX.Element => {
               </div>
               <div className="offer__rating rating">
                 <div className="offer__stars rating__stars">
-                  {сurrentOffer?.rating !== undefined && (
-                    <span style={{width: getRatingWidth(сurrentOffer.rating)}}></span>
+                  {currentOffer.rating !== undefined && (
+                    <span style={{width: getRatingWidth(currentOffer.rating)}}></span>
                   )}
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="offer__rating-value rating__value">{сurrentOffer?.rating}</span>
+                <span className="offer__rating-value rating__value">{currentOffer.rating}</span>
               </div>
               <ul className="offer__features">
                 <li className="offer__feature offer__feature--entire">
-                  {сurrentOffer?.type}
+                  {currentOffer.type}
                 </li>
                 <li className="offer__feature offer__feature--bedrooms">
-                  {`${сurrentOffer?.bedrooms} Bedrooms`}
+                  {`${currentOffer.bedrooms} Bedrooms`}
                 </li>
                 <li className="offer__feature offer__feature--adults">
-                  {`Max ${сurrentOffer?.maxAdults} adults`}
+                  {`Max ${currentOffer.maxAdults} adults`}
                 </li>
               </ul>
               <div className="offer__price">
-                <b className="offer__price-value">&euro;{сurrentOffer?.price}</b>
+                <b className="offer__price-value">&euro;{currentOffer.price}</b>
                 <span className="offer__price-text">&nbsp;night</span>
               </div>
               <div className="offer__inside">
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
-                {сurrentOffer && (
-                  <OfferInsideList offer={сurrentOffer}/>
+                {currentOffer && (
+                  <OfferInsideList offer={currentOffer}/>
                 )}
               </div>
               <div className="offer__host">
                 <h2 className="offer__host-title">Meet the host</h2>
                 <div className="offer__host-user user">
                   <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="offer__avatar user__avatar" src={сurrentOffer?.host.avatarUrl} width="74" height="74" alt="Host avatar"></img>
+                    <img className="offer__avatar user__avatar" src={currentOffer.host.avatarUrl} width="74" height="74" alt="Host avatar"></img>
                   </div>
                   <span className="offer__user-name">
-                    {сurrentOffer?.host.name}
+                    {currentOffer.host.name}
                   </span>
-                  {сurrentOffer?.host.isPro && (
+                  {currentOffer.host.isPro && (
                     <span className="offer__user-status">
                       Pro
                     </span>
@@ -163,26 +169,26 @@ const OfferPage = (): JSX.Element => {
                 </div>
                 <div className="offer__description">
                   <p className="offer__text">
-                    {сurrentOffer?.description}
+                    {currentOffer.description}
                   </p>
                 </div>
               </div>
               <section className="offer__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{сurrentReviews.length}</span></h2>
+                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{currentReviews.length}</span></h2>
                 <ul className="reviews__list">
-                  {сurrentReviews && (
-                    <ReviewsList reviews={sortReviews(Array.isArray(сurrentReviews) ? сurrentReviews : [])} />
+                  {currentReviews && (
+                    <ReviewsList reviews={sortReviews(Array.isArray(currentReviews) ? currentReviews : [])} />
                   )}
                 </ul>
                 {(authorizationStatus === AuthorizationStatus.Auth) && (
-                  <ReviewForm offerId={сurrentOffer!.id} />
+                  <ReviewForm offerId={currentOffer.id} />
                 )}
               </section>
             </div>
           </div>
-          {сurrentOffer && (
+          {currentOffer && (
             <section className="offer__map map">
-              <Map city={cityInfotmation} offers={[сurrentOffer, ...nearOffers]} activeCardId={сurrentOffer.id} />
+              <Map city={cityInfotmation} offers={[currentOffer, ...nearOffers]} activeCardId={currentOffer.id} />
             </section>
           )}
         </section>
